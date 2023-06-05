34cb1179e08c0a3b0b7111e5a21c095b1a898bb9
jenmwa <jenmwa@gmail.com>
2023-06-05 22:29:41 +0200

    cleanup

--- 3.FAQ/src/App.tsx
+++ 3.FAQ/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import Container from './components/Container';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
