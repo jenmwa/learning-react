9949ff0e80da8af7a507e3dfdf96f15fb55eb132
jenmwa <jenmwa@gmail.com>
2023-06-07 21:49:09 +0200

    axios.get on button

--- 6.sr-api/src/components/GetApi.tsx
+++ 6.sr-api/src/components/GetApi.tsx
@@ -9,34 +9,53 @@ import { ProgramPresentation } from './ProgramPresentation'
 export const GetApi = () => {
 
   const [response, setResponse] = useState<ISrResponse | undefined>(undefined);
+  const [apiFetched, setApiFetched] = useState(false);
+
   // const [program, setProgram] = useState<IProgram>();
 
   //alla program musik
   //https://api.sr.se/api/v2/programs/index?programcategoryid=5&format=json&pagination=false&indent=true
 
-  const getApiBtn = () => {
+  const getApiBtn = async () => {
     console.log('click click')
+    try {
+      const response = await axios.get<ISrResponse>('https://api.sr.se/api/v2/programs/index?programcategoryid=5&format=json&pagination=false&indent=true')
+      console.log(response.data.programs)
+      setResponse(response.data)
+      setApiFetched(true);
+    } catch (error) {
+      console.error('error', error)
+    }
   }
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await axios.get<ISrResponse>('https://api.sr.se/api/v2/programs/index?programcategoryid=5&format=json&pagination=false&indent=true')
-        console.log(response.data.programs)
-        setResponse(response.data)
-      }
-      catch (error) {
-        console.error('error', error)
-      }
-    }
-    getData()
+  const resetBtn = () => {
+    console.log('click reset')
+    setApiFetched(false);
+    setResponse(undefined);
+  }
 
-  }, []);
+  // // rendera från start
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     try {
+  //       const response = await axios.get<ISrResponse>('https://api.sr.se/api/v2/programs/index?programcategoryid=5&format=json&pagination=false&indent=true')
+  //       console.log(response.data.programs)
+  //       setResponse(response.data)
+  //     }
+  //     catch (error) {
+  //       console.error('error', error)
+  //     }
+  //   }
+  //   getData()
+
+  // }, []);
 
   return (
     <>
       <h2>Let's get all the programs in the music category!</h2>
+      {/* visa först getApiBtn, när denna har clickats och program visas, visa reset. */}
       <button onClick={getApiBtn}>CLICK HERE TO GET THEM</button>
+      <button onClick={resetBtn}>CLICK HERE TO RESET</button>
       <div className="div-container">
       {/* {response && (
         <p>{JSON.stringify(response, null, 2)}</p>
