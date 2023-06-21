ba53135e773a69349c2a2c6cbf10b2397ede85a9
jenmwa <jenmwa@gmail.com>
2023-06-21 12:33:57 +0200

    conditional rendering for shopcart added

--- 7.donut-shop-v2/src/components/ShopCart.tsx
+++ 7.donut-shop-v2/src/components/ShopCart.tsx
@@ -17,19 +17,21 @@ export const ShopCart = ({ cartFromParent }: ShopCartCompProps) => {
 
 
   //IShopCart som interface? implementera så vi har totalamount & total pieces?
-  //If inga saker i shopcart, visa Empty ShopCart msg
+  //Visa total-pieces
+  //Visa TotalAmount
+  //insert discount-area
   //själva presentations-donut-diven i egen comp
 
   const removeDonut = (id: number) => {
     console.log('remove donut: id', id)
     console.log(donutInCart);
     setDonutinCart(donutInCart.filter((donut) => donut.id !== id))
-   
+
   }
 
   const handleBuy = () => {
     console.log('buy and show buyComp')
-    
+
   }
 
   const resetBuy = () => {
@@ -38,18 +40,24 @@ export const ShopCart = ({ cartFromParent }: ShopCartCompProps) => {
 
   const html = (
     <>
-      {donutInCart.map((donut) => {
-        return <div className="cart-donut-wrapper" key={donut.id}>
-            <div className='cart-img-container'>
-            <img className="cart-img" src={donut.img}/>
-            </div>
-             
-              <p  
-            className="cart-p-text">
-              {donut.name} | {donut.amount}pcs | total: ${donut.totalAmount}</p>
-              <button className="material-symbols-outlined cart-remove" onClick={() => removeDonut(donut.id)}>delete_forever</button>
+      {donutInCart.length === 0 ? (
+        <p>No products in Cart</p>
+      ) : (
+        donutInCart.map((donut) => {
+          return (
+            <div className="cart-donut-wrapper" key={donut.id}>
+              <div className='cart-img-container'>
+                <img className="cart-img" src={donut.img} />
               </div>
-      })}
+
+              <p
+                className="cart-p-text">
+                {donut.name} | {donut.amount}pcs | total: ${donut.totalAmount}</p>
+              <button className="material-symbols-outlined cart-remove" onClick={() => removeDonut(donut.id)}>delete_forever</button>
+            </div>
+          );
+        })
+      )}
     </>
 
   )
@@ -59,9 +67,14 @@ export const ShopCart = ({ cartFromParent }: ShopCartCompProps) => {
       <section className="theShopCart">
         <p className="p-text">the shopCart</p>
         <div>{html}</div>
-        <span>Total cart:</span><br></br>
-        <button className='confirm-buy-button' onClick={handleBuy}>BUY</button><br></br>
-        <button className='cart-reset-button' onClick={resetBuy}>Reset</button>
+        {donutInCart.length !== 0 && (
+          <>
+            <span>Total cart:</span><br></br>
+            <button className='confirm-buy-button' onClick={handleBuy}>BUY</button><br></br>
+            <button className='cart-reset-button' onClick={resetBuy}>Reset</button>
+          </>
+        )}
+
       </section>
     </>
   )
