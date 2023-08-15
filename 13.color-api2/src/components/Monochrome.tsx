0046bdf5e688992eb74b0639787ada941a97c1e2
jenmwa <jenmwa@gmail.com>
2023-08-15 21:52:16 +0200

    show second monochrome scheme

--- 13.color-api2/src/components/Monochrome.tsx
+++ 13.color-api2/src/components/Monochrome.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
+import { IColorScheme } from "../Models/IColorScheme";
+import { getColorScheme } from "../Services/ColorService";
+
 interface IColorProps {
   color: string;
 }
 
 export const Monochrome = ({ color }: IColorProps) => {
 
+  const [myColorScheme, setMyColorScheme] = useState<IColorScheme | undefined>();
+  const colorId = color.replace('#', '');
+
+  useEffect(() => {
+    const getSchemeApi = async () => {
+      const response = await getColorScheme(colorId);
+      if (response) {
+        setMyColorScheme(response)
+      }
+    }
+    getSchemeApi();
+  }, [colorId])
+
   return <>
     <p>Monochrome</p>
     {color}
+    {myColorScheme ? (
+    <div>
+      <img src={myColorScheme.image?.named} />
+    </div>
+    ) : (
+      <p>Getting color information...</p>
+    )
+    }
+   
   </>
 }
